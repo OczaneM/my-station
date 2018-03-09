6b3f59fbe60ad26a63cf3fbd7df724bbd0e15d81
Oczane <oczane.rivera@gmail.com>
2018-03-09 15:35:42 -0500

    Add libraries to handle user's id_token and send token to backend

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 const path = require('path')
 const PORT = (process.env.PORT || 4001)
 
+app.use(morgan('dev'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.use(express.static(path.join(__dirname, '.')))
 
-app.use('*', (req, res) => {
+app.use('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, './index.html'))
+  next()
+})
+
+app.post('/idToken', (req, res, next) => {
+  let {idtoken} = req.body
+  res.send(idtoken)
 })
 
 const server = app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,25 @@
 'use strict'
 
+gapi.load('auth2', function() {
+  // Library loaded.
+});
+
 function onSignIn (googleUser) {
+  var profile = googleUser.getBasicProfile()
+  var email = profile.getEmail()
   var id_token = googleUser.getAuthResponse().id_token
-  $(".data").css("display","block")
-  $("#id").text("ID TOKEN: " + id_token)
+
+  $('.data').css('display', 'block')
+  $('#access').text(id_token)
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/idToken');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+    console.log('Signed in as: ' + xhr.responseText);
+  };
+  xhr.send('idtoken=' + id_token);
+
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', `https://www.google.com/m8/feeds/contacts/${email}/full?max-results=50`);
 }
